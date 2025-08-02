cb05d07516c4059872ef80cee6363182ce6b533d
sadmagrage <victorgoyapvgp09@gmail.com>
2025-08-02 11:36:02 -0300

    chore: configure db context

---
+++ Progress.API/Context/ApplicationContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Progress.API.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+    }
+}
