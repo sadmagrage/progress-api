d23a3f50d5b68f2c36706810b02478a607e48164
sadmagrage <victorgoyapvgp09@gmail.com>
2025-08-10 02:38:24 -0300

    chore: remove application context from api project

--- Progress.API/Context/ApplicationContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace Progress.API.Context
-{
-    public class ApplicationContext : DbContext
-    {
-        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
-    }
-}
