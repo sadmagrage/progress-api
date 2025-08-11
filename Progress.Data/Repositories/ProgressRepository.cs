6fcabb9c6213371514a57d94f0b281af8c9e141b
sadmagrage <victorgoyapvgp09@gmail.com>
2025-08-11 02:16:01 -0300

    feature: add progress repository

---
+++ Progress.Data/Repositories/ProgressRepository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Progress.Data.Context;
+using Progress.Domain.Entities;
+using Progress.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progress.Data.Repositories
+{
+    public class ProgressRepository : IProgressRepository
+    {
+        private readonly ApplicationContext _context;
+
+        public ProgressRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<List<ProgressEntity>> FindAll() 
+        {
+            var progress = await _context.FindAsync<List<ProgressEntity>>();
+
+            return progress;
+        }
+
+        public async Task<ProgressEntity> FindById(Guid id)
+        {
+            var progress = await _context.Progress.Where(progress => progress.Id.Equals(id)).FirstAsync();
+
+            return progress;
+        }
+
+        public async Task Save(ProgressEntity progress)
+        {
+            await _context.Progress.AddAsync(progress);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(ProgressEntity progress)
+        {
+            await _context.Progress.AddAsync(progress);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(Guid id)
+        {
+            await _context.Progress.Where(progress => progress.Id.Equals(id)).ExecuteDeleteAsync();
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
