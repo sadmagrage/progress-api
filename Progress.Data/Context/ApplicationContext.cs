ed839cfda9585adc3ea3a211c2e4f81adc487b39
sadmagrage <victorgoyapvgp09@gmail.com>
2025-08-11 02:10:07 -0300

    feature: add progress entity context

--- Progress.Data/Context/ApplicationContext.cs
+++ Progress.Data/Context/ApplicationContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Progress.Domain.Entities;
 
 namespace Progress.Data.Context
 {
     public class ApplicationContext : DbContext
     {
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        public DbSet<ProgressEntity> Progress { get; set; }
     }
 }
