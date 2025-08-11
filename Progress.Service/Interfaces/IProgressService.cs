d7105dfa64ae9e413b27f1b0ac3d142a337d915c
sadmagrage <victorgoyapvgp09@gmail.com>
2025-08-11 02:39:34 -0300

    feat: add progress service interface

---
+++ Progress.Service/Interfaces/IProgressService.cs
@@ -0,0 +1,19 @@
+﻿using Progress.Application.DTO.Requests;
+using Progress.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progress.Application.Interfaces
+{
+    public interface IProgressService
+    {
+        Task<List<ProgressEntity>> FindAll();
+        Task<ProgressEntity> FindById(Guid id);
+        Task Save(ProgressRequest progressRequest);
+        Task Update(ProgressRequest progressRequest);
+        Task Delete(Guid id);
+    }
+}
