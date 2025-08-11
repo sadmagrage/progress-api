047d37dc226757a35f67f5099260b661ffdce4ec
sadmagrage <victorgoyapvgp09@gmail.com>
2025-08-11 02:38:42 -0300

    feat: add progress request dto

---
+++ Progress.Service/DTO/Requests/ProgressRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progress.Application.DTO.Requests
+{
+    public class ProgressRequest
+    {
+        public DateTime Time { get; set; }
+        public string Type { get; set; } = "";
+    }
+}
