f05cb75f0a0c068917cbd885e9c2003295335aaf
sadmagrage <victorgoyapvgp09@gmail.com>
2025-08-11 02:07:52 -0300

    feature: add progress entity

---
+++ Progress.Domain/Entities/ProgressEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progress.Domain.Entities
+{
+    public class ProgressEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime Time { get; set; }
+        public string Type { get; set; } = "";
+    }
+}
