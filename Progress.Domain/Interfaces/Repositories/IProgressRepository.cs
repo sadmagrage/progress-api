5a315af1eb9a65fcc7a5427653b10dc965032bb0
sadmagrage <victorgoyapvgp09@gmail.com>
2025-08-11 02:38:18 -0300

    feat: add progress repository interface

---
+++ Progress.Domain/Interfaces/Repositories/IProgressRepository.cs
@@ -0,0 +1,18 @@
+﻿using Progress.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progress.Domain.Interfaces.Repositories
+{
+    public interface IProgressRepository
+    {
+        Task<List<ProgressEntity>> FindAll();
+        Task<ProgressEntity> FindById(Guid id);
+        Task Save(ProgressEntity progress);
+        Task Update(ProgressEntity progress);
+        Task Delete(Guid id);
+    }
+}
